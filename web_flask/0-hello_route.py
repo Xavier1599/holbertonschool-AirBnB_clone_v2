7d2160b9683a213c78cf4aefbe0983f966595f55
XavierPerezz <xnator9@gmail.com>
2022-09-10 22:00:32 +0000

    adding task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,6 +4,8 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,10 +13,9 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
 
