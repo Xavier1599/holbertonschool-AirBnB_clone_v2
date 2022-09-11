18d2335d6f1a7f9fabae96d32316b65b1c25475e
XavierPerezz <xnator9@gmail.com>
2022-09-11 03:24:51 +0000

    adding task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000"""
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/", strict_slashes=False)
@@ -40,10 +40,6 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template("6-number_odd_or_even.html", n=n)
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
